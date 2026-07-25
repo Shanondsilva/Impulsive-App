ec8580996df60f26e01e690cd2d5c8f5c92c7c76
Shanon Dsilva <shanondsilva2135@gmail.com>
2026-07-25 20:10:11 +0100

    build: add Firebase Storage dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,6 +139,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.functions)
+    implementation(libs.firebase.storage)
     implementation(libs.firebase.crashlytics)
     releaseImplementation(libs.firebase.appcheck.playintegrity)
     debugImplementation(libs.firebase.appcheck.debug)
